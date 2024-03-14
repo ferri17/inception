81f36b93a2b2c20a36e6f6d3ea91bfd81e6d2522
Ferran Bosch <ferranbosch17@gmail.com>
2024-03-14 17:27:47 +0100

    www.conf edit + depends

--- srcs/requirements/wordpress/tools/wordpress_start.sh
+++ srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+if [ -f /etc/php/7.4/fpm/pool.d/www.conf ]; then
+	sed -i '/^listen = /s/.*/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
+	echo "Successfully edited www.conf: Listening on port 9000."
+else
+	echo "Error: www.conf file doesn't exist"
+fi
 if [ ! -f ./wp-config.php ]
 then
 	wp core download --locale=en_US --allow-root
@@ -8,6 +14,7 @@ then
 	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASS --allow-root
 	#wp theme install twentysixteen --activate --allow-root
 fi
+sleep 100000
 #wp --info
 
 #while [ -f /etc/wordpress_start.sh ]; do
